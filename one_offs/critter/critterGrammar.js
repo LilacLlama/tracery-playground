0ad3a450f637197e2196b82bc9c403d184843d74
LilacLlama <8921522+LilacLlama@users.noreply.github.com>
2018-03-25 13:43:36 -0400

    Create critterGrammar.js
    Creating a very simple critter grammar for the critter generator one off sandbox.

---
+++ one_offs/critter/critterGrammar.js
@@ -0,0 +1,6 @@
+var critterGrammar = {
+        origin:["#adj# #location# #animal#"],
+        adj :["red", "lilac", "jumping","burrowing", "miniature", "giant"],
+        location :["mountain", "river", "plains-dweller","space-faring"],
+        animal :["donkey", "llama", "pangolin","kangaroo"]
+    };
