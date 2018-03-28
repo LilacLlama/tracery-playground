f6948cf2d6690cb1a59d0b187018c005b5d8e941
LilacLlama <8921522+LilacLlama@users.noreply.github.com>
2018-03-27 21:56:03 -0400

    Update critterGenerator.js
    I have conquered the pinterest! (Nominally). Figured out I was forgetting a click listener which in ... hindsight. Was obvious. Ha.
    Left in the hidden image hack, because well... maybe I'll use it for something else in the future.

--- one_offs/critter/critterGenerator.js
+++ one_offs/critter/critterGenerator.js
@@ -1,27 +1,33 @@
+// because this is nice.
 $.fn.exists = function () {
     return this.length !== 0;
+}
+
+function makeHiddenImg(critterText){
+
+    var id = makeCritterId(critterText);
+
+    if(!$("#"+id).exists()){
+        var hiddenImg = $("<img>").attr({
+            src:"http://fleurdellama.com/llamacode/imagemaker.php?critter="+critterText.replace(/&/g,"and"),
+            class:"d-none",
+            id:id+"-img"
+        });
+
+        $("#"+id+"-div").prepend(hiddenImg);
+    }
+
+    return true;
 }
 
 var grammar = tracery.createGrammar(critterGrammar);
 
 var toggle = true;
 
-function makeHiddenImg(critterText){
-
-    var makeId = critterText.replace(/ /g,"-");
-    makeId = makeId.replace(/&/g,"-");
-
-    if(!$("#"+makeId).exists()){
-        var hiddenImg = $("<img>").attr({
-            src:"http://fleurdellama.com/llamacode/imagemaker.php?critter="+critterText,
-            class:"d-none",
-            id:makeId
-        });
-
-        $("#critter-pen").prepend(hiddenImg);
-    }
-
-    return true;
+function makeCritterId(critterText){
+    var id = critterText.replace(/ /g,"-");
+    id = id.replace(/&/g,"-");
+    return id;
 }
 
 function makeCritters(num){
@@ -30,31 +36,33 @@ function makeCritters(num){
 
         var iconColor = toggle ? "text-dark" : "text-light";
         var color = toggle ? "bg-light" : "bg-dark text-white";
-        toggle = !toggle;
 
-    //<a href="sms://+14035550185?body=I%27m%20interested%20in%20your%20product.%20Please%20contact%20me.">Send a SMS message</a>
-    //<i class="fas fa-mobile"></i>
-
-    //<a href="https://www.pinterest.com/pin/create/button/"
-        //    data-pin-do="buttonBookmark"
-        //        data-pin-custom="true">
-        //            --YOUR CUSTOM HTML--
-        //    </a>
-
-
-
+        var pinterestId = makeCritterId(critterText) + "-pinterest";
 
         var pinLink = $("<a>").attr({
-            "onclick":"makeHiddenImg('"+critterText+"');",
+            //"onclick":"makeHiddenImg('"+critterText+"');",
             "href":"https://www.pinterest.com/pin/create/button/",
-            "data-pin-do":"buttonPin",
+            "data-pin-do":"buttonBookmark",
             "data-pin-custom":"true",
-            "data-description":"I just generated a '"+critterText+"' in this whackadoodle critter generator!",
-            "data-pin-media":"http://fleurdellama.com/llamacode/imagemaker.php?critter="+critterText,
-            "data-pin-url":"https://bit.ly/2pEkzWv"
+            "data-description":"Procedural Generated Critter. I found a '"+critterText+"' in this procedural critter generator powered by Tracery! Check out https://lilacllama.github.io/tracery-playground/one_offs/critters.html to find your own!",
+            "data-pin-media":"http://fleurdellama.com/llamacode/imagemaker.php?critter="+critterText.replace(/&/g,"and"),
+            "data-pin-url":"https://lilacllama.github.io/tracery-playground/one_offs/critters.html",
+            "id":pinterestId
         }).append($("<i>").attr({
             class:"fab fa-pinterest fa-3x text-danger"
         }));
+
+        pinLink.click(function(e, f) {
+
+            console.log(e.currentTarget.getAttribute('data-media-pin'));
+
+            PinUtils.pinOne({
+                'url': e.currentTarget.getAttribute('data-pin-url'),
+                'media': e.currentTarget.getAttribute('data-pin-media'),
+                'description': e.currentTarget.getAttribute('data-description')
+            });
+
+        });
 
 
         var smsLink = $("<a>").attr({
@@ -75,43 +83,27 @@ function makeCritters(num){
             class:"fab fa-twitter fa-3x text-primary"
         }));
 
-        //<svg height="30" width="200" xmlns:xlink="http://www.w3.org/1999/xlink">
-    //<a xlink:href="https://www.w3schools.com/graphics/" target="_blank">
-    //        <text x="0" y="15" fill="red">I love SVG!</text>
-    //    </a>
-    //    Sorry, your browser does not support inline SVG.
-    //    </svg>
-
         var critterDiv = $("<div>").attr({
             class : "critter-div col-12 col-md-4 card align-items-center p-3 " +color
         }).append($("<div class='row justify-content-center'>")
-            .append($("<svg>").attr({
-                class:"card-body text-center"
-            })
-                .append($("<text>")
-                    .attr({
-                        x:0,
-                        y:0,
-                        fill:"black",
-                        "font-size": "48px",
-                        "font-weight":"bold"
-
-                    })
-                    .text(critterText)
-                )
-            )
-        )
+            .append(
+                $("<h3>").attr({
+                        class:"card-body text-center"
+                    }).text(critterText)
+        ))
         .append($("<div class='row justify-content-center'>")
                 .append(pinLink)
                 .append(smsLink)
                 .append(tweetLink)
-                //.append(hiddenImg)
-
         );
 
         $("#critter-pen").prepend(critterDiv);
+
+
     }
 }
+
+
 
 $(document).ready(function (){
     makeCritters(15);
